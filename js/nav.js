b4b0f2e06d1c553f3513be4ef8730a3c5b7268ca
ikarasus <avryanzchandra@gmail.com>
2020-11-18 21:11:10 +0700

    change

--- js/nav.js
+++ js/nav.js
@@ -38,7 +38,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 }
             }
         };
-        xhttp.open("GET", "pages/" + page + ".html", true);
+        xhttp.open("GET", `pages/${page}.html`, true);
         xhttp.send();
     }
 
